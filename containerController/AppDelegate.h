2468c4021dad2dd36b639c0502420cbbde9f4d78
zhifeng <zhifeng@zhifengdeMacBook-Pro.local>
2014-07-31 15:52:41 +0800

    Initial Commit

---
+++ containerController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  containerController
+//
+//  Created by zhifeng on 14-7-31.
+//  Copyright (c) 2014年 zhifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
